f25b0152d87e1444d68692aac9ad70894b691b8e
KhaledElbasiouni <khaled.elbasiouni222@gmail.com>
2024-10-14 18:20:58 -0400

    Add parameters to scheduler constructor

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,7 @@
+from enums import SchedulerType, DistributionType
 class Scheduler:
     
-    def __init__(self):
+    def __init__(self, num_of_robots: int, initial_positions: float | None, robot_speeds: float | list[float], visibility_radius: float | list[float], robot_orientations: list[tuple[float, float, float]] | None, multiplicity_detection: bool, robot_colors: list[str] | None, obstructed_visibility: bool, rigid_movement: bool,probability_distribution: str = DistributionType.GAUSSIAN, scheduler_type: str =SchedulerType.ASYNC): 
         pass
         
     def get_snapshot(self) -> dict[int, tuple[tuple[float,float],str]]:
