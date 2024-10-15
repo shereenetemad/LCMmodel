58655241ad29e537a133919f945863598b517ca1
KhaledElbasiouni <khaled.elbasiouni222@gmail.com>
2024-10-15 09:51:29 -0400

    Add for-loop for simulation

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import json
+from time import sleep
 from robot import Robot
 from scheduler import Scheduler
 
@@ -11,3 +12,8 @@ print(config)
 scheduler = Scheduler(num_of_robots=config["number_of_robots"], 
                       initial_positions= config["initial_positions"],
                       robot_speeds=config["robot_speeds"])
+
+for i in range(10):
+    scheduler.handle_event()
+    sleep(1) 
+    
