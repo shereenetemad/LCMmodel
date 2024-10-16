598cb60aeb68812f9f0e55b5c0cbd1aedce844e4
KhaledElbasiouni <khaled.elbasiouni222@gmail.com>
2024-10-16 13:08:23 -0400

    Add exception handline for config file

--- run.py
+++ run.py
@@ -9,6 +9,18 @@ with open('config.json', 'r') as file:
 
 print(config)
 
+if (isinstance(config["initial_positions"], list)and config["number_of_robots"] != len(config["initial_positions"]) ):
+    raise Exception("Error in config.json: length of the initial_positions array must be the same as the value in num_of_robots")
+
+if (isinstance(config["robot_speeds"], list) and config["number_of_robots"] != len(config["robot_speeds"]) ):
+    raise Exception("Error in config.json: length of the robot_speeds array must be the same as the value in num_of_robots")
+
+if (isinstance(config["robot_orientations"], list) and config["number_of_robots"] != len(config["robot_orientations"]) ):
+    raise Exception("Error in config.json: length of the robot_orientations array must be the same as the value in num_of_robots")
+
+if (isinstance(config["robot_colors"], list) and config["number_of_robots"] != len(config["robot_colors"]) ):
+    raise Exception("Error in config.json: length of the robot_colors array must be the same as the value in num_of_robots")
+
 scheduler = Scheduler(num_of_robots=config["number_of_robots"], 
                       initial_positions= config["initial_positions"],
                       robot_speeds=config["robot_speeds"])
