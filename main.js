ac7d0f649d2bba2306161ee98cab41a3eaf78a69
KhaledElbasiouni <khaled.elbasiouni222@gmail.com>
2024-11-01 23:09:56 -0400

    Create initial files for UI

---
+++ main.js
@@ -0,0 +1,51 @@
+// @ts-nocheck
+
+window.addEventListener("resize", resizeCanvas);
+
+// Constants
+const ROBOT_SIZE = 4;
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+let window_height = window.innerHeight;
+let window_width = window.innerWidth;
+
+resizeCanvas();
+
+class Robot {
+  constructor(x, y, id, color, speed) {
+    this.id = id;
+    this.color = color;
+    this.speed;
+    this.x = x;
+    this.y = y;
+    this.radius = ROBOT_SIZE;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  update() {}
+}
+
+let robot = new Robot(0, 0, 1, "red", 1);
+robot.draw(ctx);
+
+function resizeCanvas() {
+  // This function resets the canvas
+  console.log("Resized Canvas");
+  window_height = window.innerHeight;
+  window_width = window.innerWidth;
+  canvas.width = window_width;
+  canvas.height = window_height;
+
+  // Translate the coordinate system to be in the center
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+}
